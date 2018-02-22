cafa8877b8055773131f99d8d879f6c466774c68
zmahfuz817 <zunayed.lm@gmail.com>
2018-02-22 02:17:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // write your code below!
-var name = 'Lauren Atterbury Ahmed'
-var height = '66'
+var name = "Lauren Atterbury Ahmed"
+var height = "66"
 var message = `${name} is ${height} inches tall`
 module.exports = {name, height, message}
